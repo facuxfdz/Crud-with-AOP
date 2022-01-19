0c826d0ce694085e53e521c8cae75f961deacb5a
Facundo Agustín Fernández <facuassain@gmail.com>
2022-01-18 23:17:48 -0300

    Exception catch with @AfterThrowing advice to log an error msg

--- src/main/java/com/clients/clientsspring/interceptor/AspectAfterClient.java
+++ src/main/java/com/clients/clientsspring/interceptor/AspectAfterClient.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
@@ -20,6 +21,11 @@ public class AspectAfterClient {
 
     @After("allControllersWithCustomAnnotation()")
     public void logMsgAfterExecution(JoinPoint jp) {
-        logger.info("EXECUTED METHOD NAME: " + jp.getSignature().getName());
+        logger.info("METHOD HAS BEEN EXECUTED - Method name: " + jp.getSignature().getName());
+    }
+
+    @AfterThrowing("execution(* com.clients.clientsspring.service.ClientService.updateClient(..))")
+    public void logMissingFieldsError(){
+        logger.error("All fields must be provided");
     }
 }
